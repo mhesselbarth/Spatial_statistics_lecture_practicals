e545c0111af42c045599c2bce0f1149868b3ea0e
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-03-22 10:27:41 +0100

    Update style

--- Stuff/Practical_1_Pattern.R
+++ Stuff/Practical_1_Pattern.R
@@ -1,17 +1,18 @@
 library(tidyverse)
+library(helpeR)
 
-setwd('/Users/Maximilian/ownCloud/03_Lehre/Spatial_Statistics/Data/R')
+douglas_fir <- readr::read_delim(file = "Solutions/Data/DouglasFir_Adult_vs_Seedling_OGN.txt",
+                                 delim = ';')
 
-douglas_fir <- read_delim(file = 'DouglasFir_Adult_vs_Seedling_OG-N.txt', delim = ';')
+douglas_fir <- dplyr::filter(douglas_fir, mark == 1)
 
-douglas_fir <- filter(douglas_fir, mark == 1)
-
-
-plot_pattern <- ggplot(data=douglas_fir) + 
-  geom_point(aes(x=x, y=y), shape=1) + 
+plot_pattern <- ggplot2::ggplot(data = douglas_fir) + 
+  geom_point(ggplot2::aes(x = x, y = y), shape = 1) + 
   coord_cartesian() + 
   labs(x="x [m]", y="y [m]", title="Western Hemlock - Adult trees in old-growth south") + 
   theme_bw()
 
-ggsave(filename = "Practical_1_Pattern.pdf", plot = plot_pattern, 
-       path = '/Users/Maximilian/ownCloud/03_Lehre/Spatial_Statistics/Practicals')
+helpeR::save_ggplot(plot = plot_pattern, 
+                    filename = "Practical_1_Pattern.pdf", path = "Stuff",
+                    width = 29.7, height = 21.0, units = "cm")
+
